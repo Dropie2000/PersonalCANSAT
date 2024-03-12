b5ca831c95693c98a1eeec4a83377b028891ee45
Erick Corona <erick.gonzc@gmail.com>
2024-03-11 18:57:48 -0600

    Project Structure

--- CanSAT_personal.X/nbproject/Makefile-default.mk
+++ CanSAT_personal.X/nbproject/Makefile-default.mk
@@ -57,17 +57,17 @@ OBJECTDIR=build/${CND_CONF}/${IMAGE_TYPE}
 DISTDIR=dist/${CND_CONF}/${IMAGE_TYPE}
 
 # Source Files Quoted if spaced
-SOURCEFILES_QUOTED_IF_SPACED=App/CanSAT.c
+SOURCEFILES_QUOTED_IF_SPACED=CanSAT.c
 
 # Object Files Quoted if spaced
-OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/App/CanSAT.p1
-POSSIBLE_DEPFILES=${OBJECTDIR}/App/CanSAT.p1.d
+OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/CanSAT.p1
+POSSIBLE_DEPFILES=${OBJECTDIR}/CanSAT.p1.d
 
 # Object Files
-OBJECTFILES=${OBJECTDIR}/App/CanSAT.p1
+OBJECTFILES=${OBJECTDIR}/CanSAT.p1
 
 # Source Files
-SOURCEFILES=App/CanSAT.c
+SOURCEFILES=CanSAT.c
 
 
 
@@ -94,22 +94,22 @@ MP_PROCESSOR_OPTION=18F45K50
 # ------------------------------------------------------------------------------------
 # Rules for buildStep: compile
 ifeq ($(TYPE_IMAGE), DEBUG_RUN)
-${OBJECTDIR}/App/CanSAT.p1: App/CanSAT.c  nbproject/Makefile-${CND_CONF}.mk 
-	@${MKDIR} "${OBJECTDIR}/App" 
-	@${RM} ${OBJECTDIR}/App/CanSAT.p1.d 
-	@${RM} ${OBJECTDIR}/App/CanSAT.p1 
-	${MP_CC} $(MP_EXTRA_CC_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -c  -D__DEBUG=1  -mdebugger=pickit3   -mdfp="${DFP_DIR}/xc8"  -fno-short-double -fno-short-float -memi=wordwrite -O0 -fasmfile -maddrqual=ignore -xassembler-with-cpp -mwarn=-3 -Wa,-a -DXPRJ_default=$(CND_CONF)  -msummary=-psect,-class,+mem,-hex,-file  -ginhx32 -Wl,--data-init -mno-keep-startup -mno-download -mdefault-config-bits $(COMPARISON_BUILD)  -std=c99 -gdwarf-3 -mstack=compiled:auto:auto:auto     -o ${OBJECTDIR}/App/CanSAT.p1 App/CanSAT.c 
-	@-${MV} ${OBJECTDIR}/App/CanSAT.d ${OBJECTDIR}/App/CanSAT.p1.d 
-	@${FIXDEPS} ${OBJECTDIR}/App/CanSAT.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
+${OBJECTDIR}/CanSAT.p1: CanSAT.c  nbproject/Makefile-${CND_CONF}.mk 
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/CanSAT.p1.d 
+	@${RM} ${OBJECTDIR}/CanSAT.p1 
+	${MP_CC} $(MP_EXTRA_CC_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -c  -D__DEBUG=1  -mdebugger=none   -mdfp="${DFP_DIR}/xc8"  -fno-short-double -fno-short-float -memi=wordwrite -O0 -fasmfile -maddrqual=ignore -xassembler-with-cpp -mwarn=-3 -Wa,-a -DXPRJ_default=$(CND_CONF)  -msummary=-psect,-class,+mem,-hex,-file  -ginhx32 -Wl,--data-init -mno-keep-startup -mno-download -mdefault-config-bits $(COMPARISON_BUILD)  -std=c99 -gdwarf-3 -mstack=compiled:auto:auto:auto     -o ${OBJECTDIR}/CanSAT.p1 CanSAT.c 
+	@-${MV} ${OBJECTDIR}/CanSAT.d ${OBJECTDIR}/CanSAT.p1.d 
+	@${FIXDEPS} ${OBJECTDIR}/CanSAT.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
 	
 else
-${OBJECTDIR}/App/CanSAT.p1: App/CanSAT.c  nbproject/Makefile-${CND_CONF}.mk 
-	@${MKDIR} "${OBJECTDIR}/App" 
-	@${RM} ${OBJECTDIR}/App/CanSAT.p1.d 
-	@${RM} ${OBJECTDIR}/App/CanSAT.p1 
-	${MP_CC} $(MP_EXTRA_CC_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -c   -mdfp="${DFP_DIR}/xc8"  -fno-short-double -fno-short-float -memi=wordwrite -O0 -fasmfile -maddrqual=ignore -xassembler-with-cpp -mwarn=-3 -Wa,-a -DXPRJ_default=$(CND_CONF)  -msummary=-psect,-class,+mem,-hex,-file  -ginhx32 -Wl,--data-init -mno-keep-startup -mno-download -mdefault-config-bits $(COMPARISON_BUILD)  -std=c99 -gdwarf-3 -mstack=compiled:auto:auto:auto     -o ${OBJECTDIR}/App/CanSAT.p1 App/CanSAT.c 
-	@-${MV} ${OBJECTDIR}/App/CanSAT.d ${OBJECTDIR}/App/CanSAT.p1.d 
-	@${FIXDEPS} ${OBJECTDIR}/App/CanSAT.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
+${OBJECTDIR}/CanSAT.p1: CanSAT.c  nbproject/Makefile-${CND_CONF}.mk 
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/CanSAT.p1.d 
+	@${RM} ${OBJECTDIR}/CanSAT.p1 
+	${MP_CC} $(MP_EXTRA_CC_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -c   -mdfp="${DFP_DIR}/xc8"  -fno-short-double -fno-short-float -memi=wordwrite -O0 -fasmfile -maddrqual=ignore -xassembler-with-cpp -mwarn=-3 -Wa,-a -DXPRJ_default=$(CND_CONF)  -msummary=-psect,-class,+mem,-hex,-file  -ginhx32 -Wl,--data-init -mno-keep-startup -mno-download -mdefault-config-bits $(COMPARISON_BUILD)  -std=c99 -gdwarf-3 -mstack=compiled:auto:auto:auto     -o ${OBJECTDIR}/CanSAT.p1 CanSAT.c 
+	@-${MV} ${OBJECTDIR}/CanSAT.d ${OBJECTDIR}/CanSAT.p1.d 
+	@${FIXDEPS} ${OBJECTDIR}/CanSAT.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
 	
 endif
 
@@ -130,7 +130,7 @@ endif
 ifeq ($(TYPE_IMAGE), DEBUG_RUN)
 ${DISTDIR}/CanSAT_personal.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}: ${OBJECTFILES}  nbproject/Makefile-${CND_CONF}.mk    
 	@${MKDIR} ${DISTDIR} 
-	${MP_CC} $(MP_EXTRA_LD_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -Wl,-Map=${DISTDIR}/CanSAT_personal.X.${IMAGE_TYPE}.map  -D__DEBUG=1  -mdebugger=pickit3  -DXPRJ_default=$(CND_CONF)  -Wl,--defsym=__MPLAB_BUILD=1   -mdfp="${DFP_DIR}/xc8"  -fno-short-double -fno-short-float -memi=wordwrite -O0 -fasmfile -maddrqual=ignore -xassembler-with-cpp -mwarn=-3 -Wa,-a -msummary=-psect,-class,+mem,-hex,-file  -ginhx32 -Wl,--data-init -mno-keep-startup -mno-download -mdefault-config-bits -std=c99 -gdwarf-3 -mstack=compiled:auto:auto:auto        $(COMPARISON_BUILD) -Wl,--memorysummary,${DISTDIR}/memoryfile.xml -o ${DISTDIR}/CanSAT_personal.X.${IMAGE_TYPE}.${DEBUGGABLE_SUFFIX}  ${OBJECTFILES_QUOTED_IF_SPACED}     
+	${MP_CC} $(MP_EXTRA_LD_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -Wl,-Map=${DISTDIR}/CanSAT_personal.X.${IMAGE_TYPE}.map  -D__DEBUG=1  -mdebugger=none  -DXPRJ_default=$(CND_CONF)  -Wl,--defsym=__MPLAB_BUILD=1   -mdfp="${DFP_DIR}/xc8"  -fno-short-double -fno-short-float -memi=wordwrite -O0 -fasmfile -maddrqual=ignore -xassembler-with-cpp -mwarn=-3 -Wa,-a -msummary=-psect,-class,+mem,-hex,-file  -ginhx32 -Wl,--data-init -mno-keep-startup -mno-download -mdefault-config-bits -std=c99 -gdwarf-3 -mstack=compiled:auto:auto:auto        $(COMPARISON_BUILD) -Wl,--memorysummary,${DISTDIR}/memoryfile.xml -o ${DISTDIR}/CanSAT_personal.X.${IMAGE_TYPE}.${DEBUGGABLE_SUFFIX}  ${OBJECTFILES_QUOTED_IF_SPACED}     
 	@${RM} ${DISTDIR}/CanSAT_personal.X.${IMAGE_TYPE}.hex 
 	
 else
